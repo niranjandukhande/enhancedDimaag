ee714415aa61c68842052ba5a7b65a51c33b0949
Niranjan Dukhande <progsolos@gmail.com>
2025-03-02 10:40:58 +0530

    added frontend url to cors

--- dimaag-backend/src/app.ts
+++ dimaag-backend/src/app.ts
@@ -7,7 +7,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://www.youtube.com'],
+    origin: ['http://localhost:5173', 'https://enhanced-dimaag.vercel.app/'],
     credentials: true,
   }),
 );
